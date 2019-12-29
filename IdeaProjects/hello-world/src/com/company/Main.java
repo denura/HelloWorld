504cab95149b10810ebf3ffef9473bd4863a6a40
Aleksandr Berezin <denura@users.noreply.github.com>
2019-12-29 16:05:07 +0300

    Здесь мы создаем две переменные hello и world типа String (строка) и присваиваем им значения Hello и world. Далее мы соединяем эти строки в одну с помощью + (hello + " " + world + "!") и получаем в результате одну строку, которую и выводим на экран.

--- IdeaProjects/hello-world/src/com/company/Main.java
+++ IdeaProjects/hello-world/src/com/company/Main.java
@@ -3,9 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        System.out.println("a > b = " + (a > b));
-        System.out.println("a < b = " + (a < b));
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
     }
 }
