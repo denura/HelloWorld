7e29aa62c651df1224186b2fa33362cd6b8d6be9
Aleksandr Berezin <denura@users.noreply.github.com>
2019-12-29 15:57:47 +0300

    New

--- IdeaProjects/hello-world/src/com/company/Main.java
+++ IdeaProjects/hello-world/src/com/company/Main.java
@@ -3,9 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("1.");
-        System.out.println("2..");
-        System.out.println("3...");
+        int i = 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
         System.out.println("Hello World!");
     }
 }
